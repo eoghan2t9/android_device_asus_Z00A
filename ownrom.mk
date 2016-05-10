e05b948223bffdbed0fd3b6fa54bfb4d771b4e2b
Eoghan Cunningham <eoghan61@gmail.com>
2016-05-10 20:34:34 +0100

    Update and rename cm.mk to ownrom.mk

--- ownrom.mk
+++ ownrom.mk
@@ -5,20 +5,20 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit some common CM stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ownrom/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z00A
+PRODUCT_NAME := ownrom_Z00A
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
